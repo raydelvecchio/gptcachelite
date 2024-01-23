307223bf728e7b7873ca1f78738795385ffb01bd
Raymond Del Vecchio <raydelv@MackTruck.local>
2024-01-22 20:08:05 -0800

    better testing / bug fixes

--- test/test.py
+++ test/test.py
@@ -4,9 +4,18 @@ sys.path.append(os.path.join(os.path.dirname(__file__), '../'))
 from gptcachelite import SemanticCache
 
 def test():
-    cache = SemanticCache(db_name="test", openai_key="")
-    test_messages = [{"role": "system", "content": "I am a robot"}, {"role": "user", "content": "What is a robot?"}]
-    response = cache.complete(provider='openai', model='gpt-4', messages=test_messages)
+    cache = SemanticCache(db_name="test", openai_key="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")
+    tm1 = [{"role": "system", "content": "I am a robot"}, {"role": "user", "content": "Whats a robot"}]
+    response = cache.complete(provider='openai', model='gpt-3.5-turbo', messages=tm1)
+    print(response)
+    tm2 = [{"role": "system", "content": "I am a robot"}, {"role": "user", "content": "name a robot!"}]
+    response = cache.complete(provider='openai', model='gpt-3.5-turbo', messages=tm2)
+    print(response)
+    tm3 = [{"role": "system", "content": "I am a robot"}, {"role": "user", "content": "how are olives grown"}]
+    response = cache.complete(provider='openai', model='gpt-3.5-turbo', messages=tm3)
+    print(response)
+    tm4 = [{"role": "system", "content": "I am a robot"}, {"role": "user", "content": "What is a robot?"}]
+    response = cache.complete(provider='openai', model='gpt-3.5-turbo', messages=tm4)
     print(response)
 
 if __name__ == "__main__":
